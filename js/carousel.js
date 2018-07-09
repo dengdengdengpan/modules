c1cea96739d0486879dc6f4e011df0703c1c234a
dengdengdengpan <1306631506@qq.com>
2018-07-10 00:34:08 +0800

    将轮播js部分修改为面向对象的写法

--- js/carousel.js
+++ js/carousel.js
@@ -9,117 +9,140 @@
  * 这样就是一个完整的无限轮播
  */
 
-let $ct = $('.carousel'),
-    $imgList = $ct.find('.img-list'),
-    $imgItems = $imgList.children(),
-    $nextBtn = $ct.find('.arrow-next'),
-    $preBtn = $ct.find('.arrow-pre'),
-    $pagination = $ct.find('.pagination'),
-    beginImgCounts = $imgItems.length,
-    imgWidth = $imgItems.first().width(),
-    curImgIndex = 0,
-    isRolling = false,
-    intervalId;
-
-
-// 将首尾图片所在 li 元素克隆后放到与自己位置相反处
-let $firstImg = $imgItems.first(),
-    $lastImg = $imgItems.last();
-$imgList.append($firstImg.clone());
-$imgList.prepend($lastImg.clone());
-
-// 重新设置 $imgList 的宽度
-let curImgCounts = $imgList.children().length;
-$imgList.width(imgWidth * curImgCounts);
-
-// 修改 $imgList css，使第一张图片正常显示
-$imgList.css('left', -imgWidth);
-
-// $nextBtn 和 $preBtn 点击事件
-$nextBtn.on('click',function() {
-    playNext(1);
-});
-$preBtn.on('click',function() {
-    playPre(1);
-});
-// $pagination 中小圆点的点击事件
-$pagination.on('click','li',function() {
-    let index = $(this).index();
-    if (index > curImgIndex) {
-        playNext(index - curImgIndex);
-    } else if (index < curImgIndex) {
-        playPre(curImgIndex - index)
+let Carousel = (function() {
+    function _Carousel($ct) {
+        this.$ct = $ct;
+        this.start();
+        this.bindEvent();
+        this.autoPlay();
     }
-});
+    _Carousel.prototype.start = function() {
+        this.getElements();
+        this.setVariable();
+        this.setCloneItem();  
+        this.adjustImgList();
+    };
+    _Carousel.prototype.getElements = function() {
+        this.$imgList = this.$ct.find('.img-list');
+        this.$imgItems = this.$imgList.children();
+        this.$nextBtn = this.$ct.find('.arrow-next');
+        this.$preBtn = this.$ct.find('.arrow-pre');
+        this.$pagination = this.$ct.find('.pagination');
+    };
+    _Carousel.prototype.setVariable = function() {
+        this.beginImgCounts = this.$imgItems.length;
+        this.imgWidth = this.$imgItems.first().width();
+        this.curImgIndex = 0;
+        this.isRolling = false;
+    };
+    _Carousel.prototype.setCloneItem = function() {
+        // 将首尾图片所在 li 元素克隆后放到与自己位置相反处
+        let $firstImg = this.$imgItems.first(),
+            $lastImg = this.$imgItems.last();
+        this.$imgList.append($firstImg.clone());
+        this.$imgList.prepend($lastImg.clone());
+    };
+    _Carousel.prototype.adjustImgList = function() {
+        // 重新设置 $imgList 的宽度
+        let $imgList = this.$imgList,
+            curImgCounts = $imgList.children().length;
+        $imgList.width(this.imgWidth * curImgCounts);
 
-// 设置自动滚动
-autoPlay();
-
-// 当鼠标进入图片区域则停止自动滚动
-$imgList.on('mouseenter','.item',function() {
-    stopPlay();
-});
-// 当鼠标离开图片区域则继续滚动
-$imgList.on('mouseleave','.item',function() {
-    autoPlay();
-});
-
-function playNext(n) {
-    // 优化体验，当在滚动时则不执行滚动下一个
-    if (isRolling) {
-        return;
-    }
-    isRolling = true;
-    $imgList.animate({
-        left: '-=' + imgWidth * n
-    },1000,function() {
-        curImgIndex += n;
-        // 当图片从最后一张再向后滚动时为临界条件，即curImgIndex === beginImgCounts
-        // 此时，将 $imgList 的 css 属性 left 设置为原始第一张图片所处的值
-        // 同时将 curImgIndex 设置为0
-        if (curImgIndex === beginImgCounts) {
-            $imgList.css('left', -imgWidth);
-            curImgIndex = 0;
+        // 修改 $imgList css，使第一张图片正常显示
+        $imgList.css('left', -this.imgWidth);
+    };
+    _Carousel.prototype.bindEvent = function() {
+        let carousel = this;
+        // $nextBtn 和 $preBtn 点击事件
+        this.$nextBtn.on('click',function() {
+            carousel.playNext(1);
+        });
+        this.$preBtn.on('click',function() {
+            carousel.playPre(1);
+        });
+        // $pagination 中小圆点的点击事件
+        this.$pagination.on('click','li',function() {
+            let index = $(this).index();
+            if (index > carousel.curImgIndex) {
+                carousel.playNext(index - carousel.curImgIndex);
+            } else if (index < carousel.curImgIndex) {
+                carousel.playPre(carousel.curImgIndex - index)
+            }
+        });
+        // 当鼠标进入图片区域则停止自动滚动
+        this.$imgList.on('mouseenter','.item',function() {
+            carousel.stopPlay();
+        });
+        // 当鼠标离开图片区域则继续滚动
+        this.$imgList.on('mouseleave','.item',function() {
+            carousel.autoPlay();
+        });
+    };
+    _Carousel.prototype.playNext = function(n) {
+        let carousel = this;
+        // 优化体验，当在滚动时则不执行滚动下一个
+        if (this.isRolling) {
+            return;
         }
-        isRolling = false;
-        setPagination();
-    });
-}
-
-function playPre(n) {
-    // 优化体验，当在滚动时则不执行滚动上一个
-    if (isRolling) {
-        return;
-    }
-    isRolling = true;
-    $imgList.animate({
-        left: '+=' + imgWidth * n
-    },1000,function() {
-        curImgIndex -= n;
-        // 当图片从第一张再向前滚动时为临界条件，即curImgIndex < 0
-        // 此时，将 $imgList 的 css 属性 left 设置为原始最后一张图片所处的值
-        // 同时将 curImgIndex 设置为 beginImgCounts - 1
-        if (curImgIndex < 0) {
-            $imgList.css('left', -beginImgCounts * imgWidth);
-            curImgIndex = beginImgCounts - 1;
+        this.isRolling = true;
+        this.$imgList.animate({
+            left: '-=' + this.imgWidth * n
+        }, 1000, function() {
+            carousel.curImgIndex += n;
+            // 当图片从最后一张再向后滚动时为临界条件，即curImgIndex === beginImgCounts
+            // 此时，将 $imgList 的 css 属性 left 设置为原始第一张图片所处的值
+            // 同时将 curImgIndex 设置为0
+            if (carousel.curImgIndex === carousel.beginImgCounts) {
+                carousel.$imgList.css('left', -carousel.imgWidth);
+                carousel.curImgIndex = 0;
+            }
+            carousel.isRolling = false;
+            carousel.setPagination();
+        });
+    };
+    _Carousel.prototype.playPre = function(n) {
+        let carousel = this;
+        // 优化体验，当在滚动时则不执行滚动上一个
+        if (this.isRolling) {
+            return;
         }
-        isRolling = false;
-        setPagination();
-    });
-}
+        this.isRolling = true;
+        this.$imgList.animate({
+            left: '+=' + this.imgWidth * n
+        },1000,function() {
+            carousel.curImgIndex -= n;
+            // 当图片从第一张再向前滚动时为临界条件，即curImgIndex < 0
+            // 此时，将 $imgList 的 css 属性 left 设置为原始最后一张图片所处的值
+            // 同时将 curImgIndex 设置为 beginImgCounts - 1
+            if (carousel.curImgIndex < 0) {
+                carousel.$imgList.css('left', -carousel.beginImgCounts * carousel.imgWidth);
+                carousel.curImgIndex = carousel.beginImgCounts - 1;
+            }
+            carousel.isRolling = false;
+            carousel.setPagination();
+        });
+    };
+    _Carousel.prototype.setPagination = function() {
+        this.$pagination.find('li').removeClass('active')
+                                   .eq(this.curImgIndex)
+                                   .addClass('active');
+    };
+    _Carousel.prototype.autoPlay = function() {
+        let carousel = this;
+        this.intervalId = window.setInterval(function() {
+            carousel.playNext(1);
+        }, 2500);
+    };
+    _Carousel.prototype.stopPlay = function() {
+        window.clearInterval(this.intervalId);
+    }
+    return {
+        init: function($ct) {
+            $ct.each(function(index,el) {
+                new _Carousel($(el));
+            });
+        }
+    }
+})();
 
-function setPagination() {
-    $pagination.find('li').removeClass('active')
-                          .eq(curImgIndex)
-                          .addClass('active');
-}
-
-function autoPlay() {
-    intervalId = window.setInterval(function() {
-        playNext(1);
-    },2500);
-    return intervalId;
-}
-function stopPlay() {
-    window.clearInterval(intervalId);
-}
+Carousel.init($('.carousel'));
